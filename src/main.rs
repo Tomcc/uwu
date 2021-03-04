8070d3f31aba560f50f24145b0c0a20c074001dc
Tommaso Checchi <tommaso.checchi1@gmail.com>
2021-03-04 00:46:55 -0800

    watching works!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use clap::{App, AppSettings, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use std::{
     io::{Read, Write},
     net::{SocketAddr, TcpStream},
+    path::PathBuf,
     str::FromStr,
     thread::sleep,
     time::Duration,
@@ -45,6 +47,51 @@ fn send_msg(first_msg: &str) -> anyhow::Result<()> {
     }
 }
 
+fn single_command(command: &str) -> anyhow::Result<()> {
+    let res = send_msg(command);
+
+    if res.is_ok() {
+        log::info!("Success!");
+    }
+
+    res
+}
+
+fn watch(mut path: PathBuf, delay: Duration) -> anyhow::Result<()> {
+    log::info!("Watching project at {}", path.display());
+
+    path.push("Assets");
+
+    if !path.is_dir() {
+        return Err(anyhow::format_err!(
+            "Assets dir not found at {}. Are you sure that this is a valid Unity project?",
+            path.display()
+        ));
+    }
+
+    // Create a channel to receive the events.
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut watcher = watcher(tx, delay)?;
+
+    watcher.watch(path, RecursiveMode::Recursive)?;
+
+    loop {
+        // observe the events that imply that a file is actually changed
+        match rx.recv()? {
+            DebouncedEvent::NoticeWrite(_) => {}
+            DebouncedEvent::NoticeRemove(_) => {}
+            DebouncedEvent::Create(_) => send_msg("refresh")?,
+            DebouncedEvent::Write(_) => send_msg("refresh")?,
+            DebouncedEvent::Chmod(_) => {}
+            DebouncedEvent::Remove(_) => send_msg("refresh")?,
+            DebouncedEvent::Rename(_, _) => send_msg("refresh")?,
+            DebouncedEvent::Rescan => {}
+            DebouncedEvent::Error(e, _) => Err(e)?,
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let log_env = env_logger::Env::new().default_filter_or("info");
     env_logger::init_from_env(log_env);
@@ -58,23 +105,50 @@ fn main() -> anyhow::Result<()> {
         .subcommand(SubCommand::with_name("refresh").about("Refresh all assets"))
         .subcommand(
             SubCommand::with_name("build")
-                .about("Rebuild scripts. Only compatible with Unity 2019.3+"),
+                .about("Rebuild all scripts. Only compatible with Unity 2019.3+"),
+        )
+        .subcommand(
+            SubCommand::with_name("watch")
+                .about("Automatically calls refresh if anything under /Assets/ changes")
+                .arg(
+                    Arg::with_name("PROJECT_DIR")
+                        .help("Path to the Unity project to watch")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::with_name("delay")
+                        .short("d")
+                        .long("delay")
+                        .value_name("SECONDS")
+                        .help("Only start a refresh after this many seconds")
+                        .default_value("4")
+                        .takes_value(true),
+                ),
         )
         .setting(AppSettings::ArgRequiredElseHelp);
 
     let matches = app.get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("play") {
-        send_msg("play")?;
+        single_command("play")?;
     } else if let Some(_matches) = matches.subcommand_matches("stop") {
-        send_msg("stop")?;
+        single_command("stop")?;
     } else if let Some(_matches) = matches.subcommand_matches("refresh") {
-        send_msg("refresh")?;
+        single_command("refresh")?;
     } else if let Some(_matches) = matches.subcommand_matches("build") {
-        send_msg("build")?;
-    }
+        single_command("build")?;
+    } else if let Some(matches) = matches.subcommand_matches("watch") {
+        let path = matches
+            .value_of("PROJECT_DIR")
+            .expect("Clap should require this");
 
-    log::info!("Success!");
+        let path = PathBuf::from(path);
+
+        let delay: u64 = matches.value_of("delay").unwrap().parse()?;
+
+        watch(path, Duration::from_secs(delay))?;
+    }
 
     Ok(())
 }
